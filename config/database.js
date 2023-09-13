c634cb849d25fc36d7eba4ab5298f9ae3c622df0
isreal <isrealoparanti@gmail.com>
2023-09-13 11:03:43 +0100

    initial commit

--- config/database.js
+++ config/database.js
@@ -1,9 +1,12 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports = {mongoURI:
-       'mongodb+srv://Isreal:<Goldmine10#>@cluster0.pkhckh1.mongodb.net/?retryWrites=true&w=majority'
+    //    'mongodb+srv://Isreal:<Goldmine10#>@cluster0.pkhckh1.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://Isreal:%3CGoldmine10%23%3E@cluster0.pkhckh1.mongodb.net/?retryWrites=true&w=majority'
+
     }
 }else{
     module.exports = {mongoURI:
         'mongodb://localhost:27017/vidjot-dev'   
+
     }
 }
